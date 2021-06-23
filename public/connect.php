f808d058a368f02cb68a71a55736c1362af20f38
Carolien1234 <carolien.muller@outlook.com>
2021-06-23 08:51:46 +0200

    Html and css files
    Add

---
+++ public/connect.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "DIT weerrrkkkkkk!!";
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+//     $conn = mysqli_connect("localhost", "root", "", "pracdb") or die("Connection Failed.");
+//     echo "Connection Successful!!";
+
+//     //Om Nuwe mensies by te voeg
+//     /*$stmt = $conn->prepare("INSERT INTO customers(ID, Username, Surname, Email, Cell_Num, User_Password) VALUES (?,?,?,?,?,?)");
+//     $stmt->bind_param("ssssss", $ID, $Username, $Surname, $Email, $Cellphone, $Password);
+
+
+//     //Set parameters and execute
+//     $ID = $_POST['User_ID'];
+//     $Username = $_POST['Username'];
+//     $Surname = $_POST['Surname'];
+//     $Email = $_POST['Email'];
+//     $Cellphone = $_POST['Cellphone_Number'];
+//     $Password = $_POST['Password'];
+
+//     $stmt->execute();
+
+//     //echo "New records created successfully!";
+
+//     $stmt->close();
+//     */$conn->close();
+
+// }
+?>
