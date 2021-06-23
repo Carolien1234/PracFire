83cb8ebc5a9f3625817f80ca98b35f4c430d8ba3
Carolien1234 <carolien.muller@outlook.com>
2021-06-23 17:46:55 +0200

    HTTP ajax setup

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <div style="font-size: 200px">
-      ONS KAN MORE VERDER WERK
-    </div>
+    <!-- <div style="font-size: 200px"> -->
+   <!--    ONS KAN MORE VERDER WERK -->
+   <!--  </div> -->
+    <login></login>
   </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 
+import Login from "./components/Login";
+
 export default {
   name: 'App',
   components: {
+    Login
     //HelloWorld
   }
 }
---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <button v-on:click="login()">
+    Skaap
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  methods:{
+    login(){
+      this.$http.post().then((res)=>{
+        console.log(res);
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
